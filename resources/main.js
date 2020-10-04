baad7cc9c5e5e8a583fdb8a34203b440053520c9
AlexandraUreche <70063621+AlexandraUreche@users.noreply.github.com>
2020-10-04 17:48:09 +0300

    ready for code review

--- resources/main.js
+++ resources/main.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const seeMobileWrapperBtn=document.getElementById("seeMobileWrapperBtn");
+const closeMobileMenuBtn=document.getElementById("closeMobileMenuBtn");
 seeMobileWrapperBtn.addEventListener('click', function(){
     console.log('click');
     if(MobileWrapper.classList.contains('hidden')) { 
        // console.log('YES!');
         MobileWrapper.classList.remove('hidden');
     }
-   /** else{
-        console.log('NO!');   
-    }*/
+});
+closeMobileMenuBtn.addEventListener('click', function(){
+    console.log('click');
+    MobileWrapper.classList.add('hidden');
 });
